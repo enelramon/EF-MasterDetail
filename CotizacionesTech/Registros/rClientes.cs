bacd60a87ca207badb78d85b59fd73475f08a6a2
Enel Almonte <enelramon@hotmail.com>
2017-02-27 21:13:59 -0400

    Agregando formularios que cada persona trabajara

---
+++ CotizacionesTech/Registros/rClientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CotizacionesTech.Registros
+{
+    public partial class rClientes : Form
+    {
+        public rClientes()
+        {
+            InitializeComponent();
+        }
+    }
+}
