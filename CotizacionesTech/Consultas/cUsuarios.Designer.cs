22f945bcc52f919e79903e8ffadaaff3a9987ca2
DESKTOP-4V48SB7\Equipo <esteban.smb.3008@gmail.com>
2017-03-03 01:14:20 -0400

    Entidad Usuario y Consulta Usuarios - Esteban

--- CotizacionesTech/Consultas/cUsuarios.Designer.cs
+++ CotizacionesTech/Consultas/cUsuarios.Designer.cs
@@ -28,11 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UsuariosDataGridView = new System.Windows.Forms.DataGridView();
+            this.FiltrarTextBox = new System.Windows.Forms.TextBox();
+            this.FiltrarButton = new System.Windows.Forms.Button();
+            this.FiltrarComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsuariosDataGridView
+            // 
+            this.UsuariosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsuariosDataGridView.Location = new System.Drawing.Point(12, 67);
+            this.UsuariosDataGridView.Name = "UsuariosDataGridView";
+            this.UsuariosDataGridView.Size = new System.Drawing.Size(389, 182);
+            this.UsuariosDataGridView.TabIndex = 0;
+            // 
+            // FiltrarTextBox
+            // 
+            this.FiltrarTextBox.Location = new System.Drawing.Point(148, 22);
+            this.FiltrarTextBox.Name = "FiltrarTextBox";
+            this.FiltrarTextBox.Size = new System.Drawing.Size(162, 20);
+            this.FiltrarTextBox.TabIndex = 1;
+            // 
+            // FiltrarButton
+            // 
+            this.FiltrarButton.Location = new System.Drawing.Point(326, 20);
+            this.FiltrarButton.Name = "FiltrarButton";
+            this.FiltrarButton.Size = new System.Drawing.Size(75, 23);
+            this.FiltrarButton.TabIndex = 2;
+            this.FiltrarButton.Text = "Filtrar";
+            this.FiltrarButton.UseVisualStyleBackColor = true;
+            this.FiltrarButton.Click += new System.EventHandler(this.FiltrarButton_Click);
+            // 
+            // FiltrarComboBox
+            // 
+            this.FiltrarComboBox.FormattingEnabled = true;
+            this.FiltrarComboBox.Location = new System.Drawing.Point(12, 22);
+            this.FiltrarComboBox.Name = "FiltrarComboBox";
+            this.FiltrarComboBox.Size = new System.Drawing.Size(121, 21);
+            this.FiltrarComboBox.TabIndex = 3;
+            // 
+            // cUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "cUsuarios";
+            this.ClientSize = new System.Drawing.Size(413, 261);
+            this.Controls.Add(this.FiltrarComboBox);
+            this.Controls.Add(this.FiltrarButton);
+            this.Controls.Add(this.FiltrarTextBox);
+            this.Controls.Add(this.UsuariosDataGridView);
+            this.Name = "cUsuarios";
+            this.Text = "Consulta de Usuarios";
+            this.Load += new System.EventHandler(this.cUsuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView UsuariosDataGridView;
+        private System.Windows.Forms.TextBox FiltrarTextBox;
+        private System.Windows.Forms.Button FiltrarButton;
+        private System.Windows.Forms.ComboBox FiltrarComboBox;
     }
 }
--- CotizacionesTech/Consultas/cUsuarios.cs
+++ CotizacionesTech/Consultas/cUsuarios.cs
@@ -15,5 +15,46 @@ namespace CotizacionesTech.Consultas
         {
             InitializeComponent();
         }
+
+        private void LlenarCombo()
+        {
+            FiltrarComboBox.Items.Insert(0, "UsuarioId");
+            FiltrarComboBox.Items.Insert(1, "Nombre");
+            FiltrarComboBox.Items.Insert(2, "Todo");
+
+            FiltrarComboBox.DataSource = FiltrarComboBox.Items;
+            FiltrarComboBox.DisplayMember = "Nombre";
+        }
+
+        private void Filtrar()
+        {
+            using (var db = new DAL.Repositorio<Entidades.Usuarios>())
+            {
+                if (FiltrarComboBox.SelectedIndex == 0)
+                {
+                    UsuariosDataGridView.DataSource = db.Lista(U => U.UsuarioId == Utilidades.TOINT(FiltrarTextBox.Text));
+                }
+                else if (FiltrarComboBox.SelectedIndex == 1)
+                {
+                    UsuariosDataGridView.DataSource = db.Lista(U => U.Nombre == FiltrarTextBox.Text);
+                }
+                else
+                {
+                    UsuariosDataGridView.DataSource = db.ListaTodo();
+                }
+            }
+        }
+
+        private void cUsuarios_Load(object sender, EventArgs e)
+        {
+            LlenarCombo();
+        }
+
+        private void FiltrarButton_Click(object sender, EventArgs e)
+        {
+            Filtrar();
+        }
+        
+
     }
 }
