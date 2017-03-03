ca66eb26a97dc00f75c5fd77003fc468aee3a9dd
leandrorafael_24 <leandrorafael_24@hotmail.com>
2017-03-02 22:49:46 -0400

    Compañeros lo que me toco fue hacer la consulta de Clientes la cual esta consulta por ClienteId,Nombre,Direccion,Email,Telefono,Celular,LimiteCredito.

--- CotizacionesTech/Consultas/cClientes.Designer.cs
+++ CotizacionesTech/Consultas/cClientes.Designer.cs
@@ -31,7 +31,7 @@
             this.ClientesdataGridView = new System.Windows.Forms.DataGridView();
             this.Filtrarbutton = new System.Windows.Forms.Button();
             this.BuscartextBox = new System.Windows.Forms.TextBox();
-            this.Idlabel = new System.Windows.Forms.Label();
+            this.ConsultacomboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.ClientesdataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,13 +42,13 @@
             this.ClientesdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ClientesdataGridView.Location = new System.Drawing.Point(12, 78);
             this.ClientesdataGridView.Name = "ClientesdataGridView";
-            this.ClientesdataGridView.Size = new System.Drawing.Size(965, 220);
+            this.ClientesdataGridView.Size = new System.Drawing.Size(847, 220);
             this.ClientesdataGridView.TabIndex = 0;
             // 
             // Filtrarbutton
             // 
             this.Filtrarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.Filtrarbutton.Location = new System.Drawing.Point(905, 49);
+            this.Filtrarbutton.Location = new System.Drawing.Point(787, 49);
             this.Filtrarbutton.Name = "Filtrarbutton";
             this.Filtrarbutton.Size = new System.Drawing.Size(75, 23);
             this.Filtrarbutton.TabIndex = 1;
@@ -59,33 +59,33 @@
             // BuscartextBox
             // 
             this.BuscartextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BuscartextBox.Location = new System.Drawing.Point(760, 49);
+            this.BuscartextBox.Location = new System.Drawing.Point(642, 49);
             this.BuscartextBox.Name = "BuscartextBox";
             this.BuscartextBox.Size = new System.Drawing.Size(139, 20);
             this.BuscartextBox.TabIndex = 2;
             this.BuscartextBox.TextChanged += new System.EventHandler(this.BuscartextBox_TextChanged);
             // 
-            // Idlabel
+            // ConsultacomboBox
             // 
-            this.Idlabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.Idlabel.AutoSize = true;
-            this.Idlabel.Location = new System.Drawing.Point(737, 54);
-            this.Idlabel.Name = "Idlabel";
-            this.Idlabel.Size = new System.Drawing.Size(18, 13);
-            this.Idlabel.TabIndex = 3;
-            this.Idlabel.Text = "ID";
+            this.ConsultacomboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConsultacomboBox.FormattingEnabled = true;
+            this.ConsultacomboBox.Location = new System.Drawing.Point(435, 49);
+            this.ConsultacomboBox.Name = "ConsultacomboBox";
+            this.ConsultacomboBox.Size = new System.Drawing.Size(177, 21);
+            this.ConsultacomboBox.TabIndex = 4;
             // 
             // cClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 342);
-            this.Controls.Add(this.Idlabel);
+            this.ClientSize = new System.Drawing.Size(871, 342);
+            this.Controls.Add(this.ConsultacomboBox);
             this.Controls.Add(this.BuscartextBox);
             this.Controls.Add(this.Filtrarbutton);
             this.Controls.Add(this.ClientesdataGridView);
             this.Name = "cClientes";
             this.Text = "cClientes";
+            this.Load += new System.EventHandler(this.cClientes_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ClientesdataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -97,6 +97,6 @@
         private System.Windows.Forms.DataGridView ClientesdataGridView;
         private System.Windows.Forms.Button Filtrarbutton;
         private System.Windows.Forms.TextBox BuscartextBox;
-        private System.Windows.Forms.Label Idlabel;
+        private System.Windows.Forms.ComboBox ConsultacomboBox;
     }
 }
--- CotizacionesTech/Consultas/cClientes.cs
+++ CotizacionesTech/Consultas/cClientes.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using DAL;
+
 
 namespace CotizacionesTech.Consultas
 {
@@ -11,13 +11,70 @@ namespace CotizacionesTech.Consultas
             InitializeComponent();
         }
 
+        private void cClientes_Load(object sender, EventArgs e)
+        {
+            LLenarComboBox();
+        }
+
+        public void LLenarComboBox()
+        {
+            ConsultacomboBox.Items.Insert(0, "ClienteId");
+            ConsultacomboBox.Items.Insert(1, "Nombres");
+            ConsultacomboBox.Items.Insert(2, "Direccion");
+            ConsultacomboBox.Items.Insert(3, "Email");
+            ConsultacomboBox.Items.Insert(4, "Telefono");
+            ConsultacomboBox.Items.Insert(5, "Celular");
+            ConsultacomboBox.Items.Insert(6, "LimiteCredito");
+
+            ConsultacomboBox.DataSource = ConsultacomboBox.Items;
+            ConsultacomboBox.DisplayMember = "ClienteId";
+        }
+
+        public void SeleccionarOpcionCombo()
+        {
+            using (var consulta = new DAL.Repositorio<Entidades.Clientes>())
+            {
+                if (ConsultacomboBox.SelectedIndex == 0)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.ClienteId == Convert.ToInt32(BuscartextBox.Text));
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 1)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.Nombres == BuscartextBox.Text);
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 2)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.Direccion == BuscartextBox.Text);
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 3)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.Email == BuscartextBox.Text);
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 4)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.Telefono == BuscartextBox.Text);
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 5)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.Celular == BuscartextBox.Text);
+                }
+
+                if (ConsultacomboBox.SelectedIndex == 6)
+                {
+                    ConsultacomboBox.DataSource = consulta.Lista(p => p.LimiteCredito == Convert.ToInt32(BuscartextBox.Text));
+                }
+            }
+        }
+
         private void Filtrarbutton_Click(object sender, EventArgs e)
         {
 
-            using (var consulta = new Repositorio<Entidades.Clientes>())
-            {
-                ClientesdataGridView.DataSource = consulta.Lista(p=> p.ClienteId == Convert.ToInt32(BuscartextBox.Text));
-            }
+            SeleccionarOpcionCombo();
 
         }
 
@@ -25,5 +82,7 @@ namespace CotizacionesTech.Consultas
         {
 
         }
+
+
     }
 }
